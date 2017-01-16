847b5ed464629aca2626f1a5a6c38e36cdc13482
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-16 15:16:00 -0500

    DOB calculator

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "what is your name?"
 name = gets.chomp
 puts "hello #{name}"
+
+# age calculation
+puts "how old are you?"
+age = gets.chomp
+dob = 2017 - age.to_i
+
+puts "you were born in... #{dob}"
