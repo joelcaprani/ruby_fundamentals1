cb0c3410a3197078f092ebccee8046270d9f6329
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-16 16:26:10 -0500

    exercise4 finished

---
+++ exercise4.rb
@@ -0,0 +1,13 @@
+(1..100).each do |number|
+  if {number % 3 == 0} && (number % 5 == 0)
+    puts "bitmaker"
+
+  elsif (number % 3 == 0)
+    puts "bit"
+
+        (number % 5 ==0)
+        puts "maker"
+
+else
+  puts "number"
+end
